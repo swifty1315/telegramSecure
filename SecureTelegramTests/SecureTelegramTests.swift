fce91189878b6379303d93c8104c2d1363c9b06b
borshchov <ilya.borshchov@gmail.com>
2026-04-21 23:37:52 +0300

    Initial Commit

---
+++ SecureTelegramTests/SecureTelegramTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecureTelegramTests.swift
+//  SecureTelegramTests
+//
+//  Created by Ilya on 21.04.2026.
+//
+
+import Testing
+@testable import SecureTelegram
+
+struct SecureTelegramTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
